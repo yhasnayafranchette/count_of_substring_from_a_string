9c9aba5614a9ba2f0d2cb4932a7ae9096b66e9e3
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 20:39:16 +0800

    instructions

--- count_substring_from_string.py
+++ count_substring_from_string.py
@@ -1,2 +1,6 @@
 # Exercise 7: Return the count of a given substring from a string
 # Write a program to find how many times substring “Emma” appears in the given string.
+
+#Count how many times the substring "Emma" appeared
+
+#Print the number of occurrences
