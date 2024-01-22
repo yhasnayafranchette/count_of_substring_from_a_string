f7bb7f7f8a9e708e1f717df644130b50f228d22f
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 20:44:20 +0800

    string and display

--- count_substring_from_string.py
+++ count_substring_from_string.py
@@ -2,5 +2,9 @@
 # Write a program to find how many times substring “Emma” appears in the given string.
 
 #Count how many times the substring "Emma" appeared
+str_x = "Emma is good developer. Emma is a writer"
+print("Given String: ", str_x)
 
 #Print the number of occurrences
+count_substring = str_x.count("Emma")
+print(f"Emma appeared {count_substring} times")
