6f459581b66ad8bea57a7dc1527d0164ad7433da
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 20:52:36 +0800

    final

--- count_substring_from_string.py
+++ count_substring_from_string.py
@@ -1,10 +1,10 @@
 # Exercise 7: Return the count of a given substring from a string
 # Write a program to find how many times substring “Emma” appears in the given string.
 
-#Count how many times the substring "Emma" appeared
+#Show and display the given string
 str_x = "Emma is good developer. Emma is a writer"
 print("Given String: ", str_x)
 
-#Print the number of occurrences
+#Count and print the number of occurrences
 count_substring = str_x.count("Emma")
 print(f"Emma appeared {count_substring} times")
